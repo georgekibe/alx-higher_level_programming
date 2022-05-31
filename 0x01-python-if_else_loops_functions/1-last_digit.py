2cbcc6026c73e2753935c1d205d49a16133f5ccc
George Kibe <kiberenge94@gmail.com>
2022-06-01 01:21:34 +0300

    added functionality for negative numbers on task 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 lastDigit = number % 10
+forNegative = 10 - lastDigit
 if number > 0 and lastDigit > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 elif number > 0 and lastDigit <= 5:
     print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
 elif lastDigit == 0:
     print(f"Last digit of {number} is {lastDigit}")
+elif number < 0 and forNegative > 5:
+    print(f"Last digit of {number} is {-1*forNegative} and is greater than 5")
+elif number < 0 and forNegative <= 5:
+    print(f"Last digit of {number} is {-1*forNegative} and is less than 6 and not 0")
